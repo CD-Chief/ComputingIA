d34aaab178a4077bdcec641c87175e747fe0e78f
Zephrio <76012925+Zephrio@users.noreply.github.com>
2022-09-13 22:42:09 +0200

    Added base
    Mostly added classes and started on Colour theory

---
+++ clothes.js
@@ -0,0 +1,39 @@
+//Colour function
+function colourTheory(colour, technique){
+    //technique mening complimentary, analogous etc...
+    //would return 
+    return;
+}
+
+//Class to create different clothes
+class Clothes{
+
+    constructor(name, lightness, occasion){
+        this.name = name;
+        //lighness is supposed to mean how suitable the clothing is for how weather, i.e less material, thinner fabric
+        this.lightness = lightness;
+        //occasion literally refers to the occasion it is to be used in, i.e formal, fancy, casual etc
+        this.occasion = occasion;
+    }
+
+} 
+
+
+//Inheritance to make different types of clothes
+class Jacket extends Clothes{
+    type = "jacket";
+}
+
+class Tshirt extends Clothes{
+    type = "tshirt";
+}
+
+class Pants extends Clothes{
+    type = "pants";
+}
+
+class Shoes extends Clothes{
+    type = "shoes";
+}
+
+let winterJacket = new Jacket("Winter Jacket", 0, "Casual");
