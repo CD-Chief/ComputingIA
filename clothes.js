b8bb43a1f665c570a37078a1e580a3d2255efe85
Zephrio <76012925+Zephrio@users.noreply.github.com>
2022-09-15 21:03:13 +0200

    Different approach to adding clothes
    also added new file with predetermined values

--- clothes.js
+++ clothes.js
@@ -5,40 +5,59 @@ function colourTheory(colour, technique){
     return;
 }
 
+//add clothes
+function addClothing(Array, clothing, type, colour1, colour2, lightness, occasion){
+    Array.push(new Clothes(clothing, type, colour1, colour2, lightness, occasion));
+}
+
 //Class to create different clothes
 class Clothes{
-    Colour1 = null;
-    Colour2 = null;
 
-    constructor(name, lightness, occasion){
+    constructor(name, type, colour1, colour2, lightness, occasion){
         this.name = name;
-        //lighness is supposed to mean how suitable the clothing is for how weather, i.e less material, thinner fabric
+        this.type = type;
+        this.colour1 = colour1;
+        this.colour2 = colour2;
         this.lightness = lightness;
-        //occasion literally refers to the occasion it is to be used in, i.e formal, fancy, casual etc
         this.occasion = occasion;
     }
 
-    addClothing(Array, clothing, colour1, colour2){
+    //if clothing aligns with what is in the data then assign pre determined values for lightness and occasion
+    switch (this,type) {
+        case "jacket":
+            if (Jackets.includes(this.name)){
+                let temp = Jackets.find(this.name);
+                this.lightness = Jackets[temp][1];
+                this.occasion = Jackets[temp][2];
+            }
+            break;
 
+        case "top":
+            if (Tops.includes(this.name)){
+                let temp = Tops.find(this.name);
+                this.lightness = Tops[temp][1];
+                this.occasion = Tops[temp][2];
+            }
+            break;
+
+        case "pants":
+            if (Pants.includes(this.name)){
+                let temp = Pants.find(this.name);
+                this.lightness = Pants[temp][1];
+                this.occasion = Pants[temp][2];
+            }
+            break;
+
+        case "shoes":
+            if (Shoes.includes(this.name)){
+                let temp = Shoes.find(this.name);
+                this.lightness = Shoes[temp][1];
+                this.occasion = Shoes[temp][2];
+            }
+            break;
+
+        default:
+            break;
     }
-
 } 
 
-//Inheritance to make different types of clothes
-class Jacket extends Clothes{
-    type = "jacket";
-}
-
-class Tshirt extends Clothes{
-    type = "tshirt";
-}
-
-class Pants extends Clothes{
-    type = "pants";
-}
-
-class Shoes extends Clothes{
-    type = "shoes";
-}
-
-let winterJacket = new Jacket("Winter Jacket", 0, "Casual");
