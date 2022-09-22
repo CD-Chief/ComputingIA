40e33b49bf86dac3b1c0b2ac17d17c9c0cbdb95b
Zephrio <chief.d.osei@gmail.com>
2022-09-22 23:56:38 +0200

    switched to arrays instead of JSON

--- clothes.js
+++ clothes.js
@@ -7,7 +7,9 @@ function colourTheory(colour, technique){
 
 //add clothes
 function addClothing(Array, clothing, type, colour1, colour2, lightness, occasion){
-    Array.push(new Clothes(clothing, type, colour1, colour2, lightness, occasion));
+    let newClothing = new Clothes(clothing, type, colour1, colour2, lightness, occasion)
+    newClothing.attributes();
+    Array.push(newClothing);
 }
 
 //Class to create different clothes
@@ -24,11 +26,18 @@ class Clothes{
     }
 
     //if clothing aligns with what is in the data then assign pre determined values for lightness and occasion
-    attributes(clothing,type){
+    attributes(clothing){
+        let tempArr = findMatch(clothing);
+        let firstIterate = tempArr[0];
+        let secondIterate = tempArr[1];
         currentName = clothing.name;
         currentLightness = clothing.lightness;
         currentOccasion = clothing.occasion;
         currentLength = clothing.length;
+
+        currentLightness = allCLothes[firstIterate][secondIterate][1];
+        currentOccasion = allCLothes[firstIterate][secondIterate][2];
+        currentLength = allCLothes[firstIterate][secondIterate][3];
     }
 } 
 
--- savedclothes.js
+++ savedclothes.js
@@ -1,48 +1,23 @@
-/* 
-const Jackets = [
-    {"name" : "Cardigan", "Lightness" : 3, "Occasion" : "Casual"},
-    {"name" : "Winter Jacket", "Lightness" : 0, "Occasion" : "Casual",},
-]
+//[name, lightness, occasion, length]
+const Jackets = [ 
+    ["Cardigan", 4,"Casual", true],
+    ["Winter Jacket", 0, "Casual", true]
+];
 
 const Shirts = [
-    {"name" : "Shirt", "Lightness" : 8, "Occasion" : "Formal"}
-]
+    
+];
 
 const Pants = [
-    {"name" : "Joggers", "Lightness" : 4, "Occasion" : "Casual"}
-]
+    
+];
 
-const Shoes = [
-    {"name" : "Boots", "Lightness" : 2, "Occasion" : "Casual",},
-]
+const Skirts = [
+    
+];
 
+const Dresses = [
+    
+];
 
-
-
-const allClothes = [
-    //Name, Lightness, Occasion, Length
-    //{"name" : "", lightness : , length : , occasion ; "" , },
-
-    {
-    "Jackets":[
-        {"name":"Cardigan", "lightness":3, "length":true, "occasion":"Casual"},
-        {"name" :"Winter Jacket", lightness :0 , length :true, occasion :"Casual"}
-    ],
-    "Shirts":[
-        {}
-    ],
-    "Pants":[
-        {}
-    ],
-    "Skirt":[
-        {}
-    ],
-    "Shoes":[
-        {}
-    ],
-    "Dress":[
-        {}
-    ],
-}
-]
-*/
+const allCLothes = [Jackets, Shirts, Pants, Skirts, Dresses];
