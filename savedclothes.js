fdf9177a06fea84116e6d76eb95dcf24cf1c1302
Zephrio <chief.d.osei@gmail.com>
2022-09-27 15:29:59 +0200

    further changes

--- savedclothes.js
+++ savedclothes.js
@@ -5,40 +5,45 @@ const Jackets = [
 ];
 
 const Shirts = [
-    
+    ["T-Shirt", 8,"Casual", false]
 ];
 
 const Pants = [
-    
+    ["Dress Pants", 5,"Fancy", true]
 ];
 
 const Skirts = [
-    
+    ["Mini", 6,"Party", false]
 ];
 
 const Dresses = [
-    
+    ["Apron", 7,"Casual", null]
 ];
 
 const Shoes = [
-    
+    [""]
 ];
 
 const allCLothes = [Jackets, Shirts, Pants, Skirts, Shoes, Dresses];
 
 function countClothes(){
     let count = 0;
-    for (let i = 0; i <= 6; i++){
-        count += (allCLothes[i].length);
-    }
+    count += Jackets.length;
+    count += Shirts.length;
+    count += Pants.length;
+    count += Skirts.length;
+    count += Dresses.length;
+    count += Shoes.length;
     return count;
 }
 
 function findMatch(clothing){
-    count = countClothes;
+    count = countClothes();
+    console.log(count);
     for (let i = count ; i > -1 ; i--){
         for (let u = allCLothes[count].length ; u > -1 ; u--){
             if (clothing = allCLothes[i][u][0]){
+                console.log("found")
                 return (temp = [i,u]);
             }
         }
