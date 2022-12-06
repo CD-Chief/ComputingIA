e2d1b2a47118da1a1e3d908ce152344c56e389b1
Zephrio <chief.d.osei@gmail.com>
2022-12-06 14:55:50 +0100

    first part of configure function for colour

--- closet.js
+++ closet.js
@@ -3,21 +3,20 @@ class Closet{
         this.Owner = Owner;
         this.Storage = [];
     }
-
     //Add clothing that already exists
     addClothing(Clothing){
         this.Storage.push(Clothing);
     }
-
     //Create new clothing and add it
     addNewClothing(clothing, type, colour, coverage, occasion){
         this.Storage.push(new Clothes(clothing, type, colour, coverage, occasion));
     }
 
+    
     showAttributes(Index){
         if (this.Storage[Index]){
             let temp = this.Storage[Index];
-            console.log("Name: " + temp.name + " │type: " + temp.type + " │Colour: " + temp.colour1 + " + " + temp.colour2 + " │Coverage: " + temp.coverage + " │Occasion: " + temp.occasion);
+            console.log("Name: " + temp.name + " │type: " + temp.type + " │Colour: " + temp.colour.name1 + " + " + temp.colour.name2 + " │Coverage: " + temp.coverage + " │Occasion: " + temp.occasion);
         }
     }
 
@@ -27,10 +26,20 @@ class Closet{
         }
     }
 
+
+    // CREATING OUTFITS SECTION
+
+    //Two rows so that the second row shows which colour is directly opposite of that of the first
+    colourWheel = [
+        ["YG","Y","YO","O","RO","R","RP","P","BP","B","BG","G"],
+        ["RP","P","BP","B","BG","G","YG","Y","YO","O","RO","R"]
+    ]
+
+
     //Each of these functions have to include not creating older outfits
     //Create outfit with colour theory
     colourOutfit(colour, technique){
-        //technique mening complimentary, analogous etc...
+        //technique meaning complimentary, analogous etc...
         //would return array of pices of clothing 
         return;
     }
--- colour.js
+++ colour.js
@@ -1,11 +1,13 @@
 //CLass to create objects which store hexadecimal, name and brightness of colour
+
+
 class Colour{
-    constructor(colourHex1, colourName1, colourHex2, colourName2, brightness){
+    constructor(Hue1, colourName1, Hue2, colourName2, brightness){
         //For the two dominant colours in each piece of clothing
-        this.hex1 = colourHex1;
+        this.Hue1 = Hue1;
         this.name1 = colourName1;
 
-        this.hex2 = colourHex2;
+        this.Hue2 = Hue2;
         this.name2 = colourName2;
 
         this.brightness = brightness;
@@ -14,19 +16,59 @@ class Colour{
     //If one of the parameters is null, it is filled by converting the other value into its type
     configure(){
         //First colour
-        if (this.hex1 == null){
-            //Code to turn colour name in to hex
+        if (this.Hue1 == null){
+            //Code to turn colour name in to hue
+            let temp = this.name1;
+            let temp2;
+            switch(temp){
+                case "R":
+                    temp2 = 0
+                    break;
+                case "RO":
+                    temp2 = 30
+                    break;
+                case "O":
+                    temp2 = 60
+                    break;
+                case "YO":
+                    temp2 = 90
+                    break;
+                case "Y":
+                    temp2 = 120
+                    break;
+                case "YG":
+                    temp2 = 150
+                    break;
+                case "G":
+                    temp2 = 180
+                    break;
+                case "BG":
+                    temp2 = 210
+                    break;
+                case "B":
+                    temp2 = 240
+                    break;
+                case "BP":
+                    temp2 = 270
+                    break;
+                case "P":
+                    temp2 = 300
+                    break;
+                case "RP":
+                    temp2 = 330
+                    break;
+            }
         }else if(this.name1 = null){
-            //Code to turn hex into colour name
+            //Code to turn hue into colour name
         }else{
             console.log("Failed Configuration of Colour 1")
         }
 
         //For Second colour
-        if (this.hex2 == null){
-            //Code to turn colour name in to hex
+        if (this.Hue2 == null){
+            //Code to turn colour name in to hue
         }else if(this.name2 = null){
-            //Code to turn hex into colour name
+            //Code to turn hue into colour name
         }else{
             console.log("Failed Configuration of Colour 2")
         }
