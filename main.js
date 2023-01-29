0034261aa2bacdc62f1c8112d96957179224f02b
Zephrio <76012925+Zephrio@users.noreply.github.com>
2023-01-29 20:37:02 +0100

    testing

---
+++ main.js
@@ -0,0 +1,5 @@
+let Chief = new User("Chief", "12345", "chief.d.osei@gmail.com");
+Chief.addCloset;
+let blazercol = new Colour(20, null, 100, 65, 180, null, 100, 85)
+Chief.closet.addNewClothing("Blazer", "jacket", blazercol, null, null);
+Chief.closet.Storage[0].attributes;
